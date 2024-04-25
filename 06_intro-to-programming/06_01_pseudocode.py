a680983dd6687c75c59eed64d849260aec97890b
Amber <adm2037@gmail.com>
2024-04-25 16:21:55 -0500

    Completed labs for section 6

--- 06_intro-to-programming/06_01_pseudocode.py
+++ 06_intro-to-programming/06_01_pseudocode.py
@@ -6,3 +6,8 @@
 # Break this task up into a couple of steps of pseudocode
 # and write the pseudocode below in code comments.
 # You don't need to write any functional code, just map out the steps.
+
+# Create a dictionary of adoptable kittens to share with friends as key:value pairs
+# For keys type each of their names followed by values that list if they are adoptable or not
+# i.e. kittens_for_sale = { "Pumpkin": "Adoptable", ... } 
+# Next create prompt that asks friends which ones they think are cute
--- 06_intro-to-programming/06_05_program_pseudocode.py
+++ 06_intro-to-programming/06_05_program_pseudocode.py
@@ -6,3 +6,30 @@
 # You can also add real Python code after learning the necessary concepts.
 # For now, just practice breaking larger tasks into smaller steps
 # and writing out your thoughts in pseudocode.
+
+# A shopping list calculator I built for one of the later sections of this course 
+# since I prefer to work on the labs after completing several sections:
+
+# Sephora shopping list accounting for discounted prices
+# Create dictionary of hypothetical cost for items (in dollars)
+prices = {
+  "Refy Lip Sculpt Kit": 26.00,
+  "Nars Soft Matte Concealer": 26.00,
+  "Kaima Cosmetics Glitter": 15.00,
+  "Lilly Lashes Inviting": 12.00,
+  "Refy Brow Gel": 25.00
+}
+
+print(prices.keys())
+print(prices.values())
+
+# Loop through items - only apply 25% discount to merch under $20.00
+discounted_prices = {k:v - v * .25 for (k,v) in prices.items() if v<=20.00}
+
+print(discounted_prices) # 11.25, 9.00 new prices
+
+# update first dictionary with new updated values
+prices.update(discounted_prices)
+
+# Display results
+print(prices)
