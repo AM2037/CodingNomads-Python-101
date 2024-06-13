bcacab9cd3d716392b0ace1b7152a8e43d1b57e4
Amber <adm2037@gmail.com>
2024-06-13 17:24:05 -0500

    Section 12 input replace lab: find all occurrences of first letter

--- 12_user-input-string-formatting/12_02_input_replace.py
+++ 12_user-input-string-formatting/12_02_input_replace.py
@@ -4,3 +4,28 @@
 # String input: more python programming please
 # Symbol input: §
 # Result: §ore python progra§§ing please
+
+# Prompt user for string of words
+words = input("Please enter a random sentence: ")
+
+# Prompt user for symbol
+sym = input("Please enter a symbol: ")
+
+# Identify value of first letter
+first = words[0]
+
+# Find all occurrences of the first letter
+print([i for i, x in enumerate(words) if x == first])
+
+
+
+
+
+        
+
+
+
+
+
+
+
