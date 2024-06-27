9f6ff8f96b6046126beb823dfeb23c5c4a49f5c9
Amber <adm2037@gmail.com>
2024-06-27 16:11:07 -0500

    Completed section 12 divisible lab

--- 12_user-input-string-formatting/12_03_divisible.py
+++ 12_user-input-string-formatting/12_03_divisible.py
@@ -2,13 +2,11 @@
 # from the user and determines whether it is divisible by 3 using an `if` statement.
 # Print the result.
 
-# Write a program that takes a number between 1 and 1,000,000,000
+# Prompt user for number between 1 and 1,000,000,000
 num = int(input("Please enter a number between 1 and 1,000,000,000: "))
 
-# from the user and determines whether it is divisible by 3 using an `if` statement.
-# Print the result.
-if num % 3:
+# Check if divisible by 3
+if (num % 3) == 0:
     print(num)
 else:
-    print("This number is not divisible by 3!") 
-# TODO: DEBUG -- tried 26 and it's returning 26 even though not divisible by 3
+    print("This number is not divisible by 3!")
