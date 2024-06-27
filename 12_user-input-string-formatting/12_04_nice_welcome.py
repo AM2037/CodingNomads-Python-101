3e88ad5986bc63cd85b79c4ff9c51105230e8b3d
Amber <adm2037@gmail.com>
2024-06-27 16:23:12 -0500

    Need to debug name check section

--- 12_user-input-string-formatting/12_04_nice_welcome.py
+++ 12_user-input-string-formatting/12_04_nice_welcome.py
@@ -1,5 +1,12 @@
 # Ask the user to input their name. Then print a nice welcome message
 # that welcomes them personally to your script.
+name = input("Please enter your name: ")
+
 # If a user enters more than one name, e.g. "firstname lastname",
 # then use only their first name to overstep some personal boundaries
 # in your welcome message.
+if len(name).split() > 1: # AttributeError: 'int' object has no attribute 'split'
+    last_name = name[1]
+    print(f"Welcome {last_name}.")
+else:
+     print(f"Welcome {name}.")
