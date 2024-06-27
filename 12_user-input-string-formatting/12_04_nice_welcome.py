343e8ffb655ff97f0020921119a2f850dc011027
Amber <adm2037@gmail.com>
2024-06-27 16:32:30 -0500

    Fixed check for first and/or last names

--- 12_user-input-string-formatting/12_04_nice_welcome.py
+++ 12_user-input-string-formatting/12_04_nice_welcome.py
@@ -1,12 +1,16 @@
+import re
+
 # Ask the user to input their name. Then print a nice welcome message
 # that welcomes them personally to your script.
 name = input("Please enter your name: ")
 
-# If a user enters more than one name, e.g. "firstname lastname",
-# then use only their first name to overstep some personal boundaries
-# in your welcome message.
-if len(name).split() > 1: # AttributeError: 'int' object has no attribute 'split'
-    last_name = name[1]
-    print(f"Welcome {last_name}.")
-else:
-     print(f"Welcome {name}.")
+# Check if multiple words in string i.e. first and last names
+name_length = len(name)
+
+# Find # of spaces aka seperator between words
+spaces = len(re.findall(r'\s+', name))
+
+if (spaces + 1) == 1:
+    print("Only first name given")
+else: 
+    print("This is a first and last name.")
