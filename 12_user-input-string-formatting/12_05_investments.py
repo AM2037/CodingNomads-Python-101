17508603eaf4d8d11c7d4e29e83a9c5f1bc1a0e2
Amber <adm2037@gmail.com>
2024-06-28 15:51:51 -0500

    Completed section 12 investments lab: added futures calculation and printed result

--- 12_user-input-string-formatting/12_05_investments.py
+++ 12_user-input-string-formatting/12_05_investments.py
@@ -7,5 +7,17 @@
 
 # Prompt user for the following to be used in the formula shown above
 present_value = int(input("Please enter amount you plan to  invest: "))
-int_rate = float(input("What is the interest rate? "))
-num_years = int(input("How long do you plan to invest, in years? "))
+int_rate_prompt = float(input("What is the interest rate? "))
+# Convert interest rate response into percentage
+int_rate = int_rate_prompt / 100
+num_years = int(input("How long do you plan to invest, in years?:  "))
+
+# Formula:
+# future value = present value x (1 + (interest rate * n)) where n = num years
+
+# Calculate appreciation rate first (second half of equation)
+app_rate = 1 + (int_rate * num_years)
+future_value = present_value * app_rate
+
+# Print result
+print(f"The estimated future value of this investment is: {future_value}")
