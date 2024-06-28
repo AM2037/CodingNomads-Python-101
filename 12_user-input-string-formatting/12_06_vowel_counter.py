45604c40f84d23a5a865af40c38ef81cc6e6d57b
Amber <adm2037@gmail.com>
2024-06-28 16:36:47 -0500

    Completed section 12 vowel counter lab: removed unecessary previous counter function + else block of new vowel_freq function -- code working now

--- 12_user-input-string-formatting/12_06_vowel_counter.py
+++ 12_user-input-string-formatting/12_06_vowel_counter.py
@@ -2,19 +2,10 @@
 # and prints a total count of how often each individual vowel appeared.
 
 # Prompt user for a sentence
-input_string = str(input("Please enter any sentence here."))
+input_string = str(input("Please enter any sentence here: "))
 
 # Declare vowels
 vowels = "aeiou"
-
-def count_vowels(input_string, vowels):
-
-# Trying comprehension instead for overall vowel count
-    vowel_counter = [char for char in input_string if char in vowels]
-    total_count = len(vowel_counter)
-    print(f"The total number of vowels is: {total_count}")
-
-count_vowels(input_string, vowels)
 
 freq = {}.fromkeys(vowels, 0)
 
@@ -23,10 +14,9 @@ def vowel_freq(input_string, vowels):
     for char in input_string:
         if char in vowels:
             freq[char] += 1
-        else:
-            freq[char] = 1
 
     return freq
 
-frequency = vowel_freq(input_string, vowels)
+# frequency = vowel_freq(input_string, vowels)
+frequency = vowel_freq(input_string, freq)
 print(f"The total number of times each vowel appears is: {frequency}")
