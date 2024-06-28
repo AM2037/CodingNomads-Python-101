6cef5174b6619abd2e509422707e3803e2bc5d01
Amber <adm2037@gmail.com>
2024-06-28 16:00:05 -0500

    Need to debug TypeError

--- 12_user-input-string-formatting/12_06_vowel_counter.py
+++ 12_user-input-string-formatting/12_06_vowel_counter.py
@@ -1,2 +1,19 @@
 # Write a script that takes a string input from a user
 # and prints a total count of how often each individual vowel appeared.
+
+# Prompt user for a sentence
+input_string = str(input("Please enter any sentence here."))
+
+# Initialize vowel counter
+vowel_count = 0
+vowels = ['a', 'e', 'i', 'o', 'u']
+
+def count_vowels(char, vowels): #could change to match call below if doesn't work
+
+    for char in input_string:
+        if char in vowels: # TypeError: argument of type 'int' is not iterable
+           vowel_count += 1
+
+total_vowel_count = count_vowels(input_string, vowel_count) # same TypeError here too
+
+print(f"The total number of vowels is: {total_vowel_count}")
