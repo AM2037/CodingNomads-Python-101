2505308c948e9334838fee5b4f59240cc5fa002f
Amber <adm2037@gmail.com>
2024-06-28 15:30:09 -0500

    Prompted user for required information

--- 12_user-input-string-formatting/12_05_investments.py
+++ 12_user-input-string-formatting/12_05_investments.py
@@ -4,3 +4,8 @@
 # 3. number of years to invest
 #
 # Calculate the future values and print them to the console.
+
+# Prompt user for the following to be used in the formula shown above
+present_value = int(input("Please enter amount you plan to  invest: "))
+int_rate = float(input("What is the interest rate? "))
+num_years = int(input("How long do you plan to invest, in years? "))
