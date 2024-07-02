ed87c809f64cdae121bd5c6538ca91fef1bf3f52
Amber <adm2037@gmail.com>
2024-07-02 17:44:07 -0500

    Mostly working now but need to fix 2nd prompt repeating

--- 12_user-input-string-formatting/12_09_number_search.py
+++ 12_user-input-string-formatting/12_09_number_search.py
@@ -5,14 +5,15 @@
 # Prompt user for number within specified range
 num = int(input("Please enter a number between 0 and 1,000,000,000: "))
 
-# If user enters number:
+# Check if number in range
 while True:
-    # and number is within range:
-    for i in range(0,1000000001):
-        # find and print number
-        num_search = num.find(i) # fix AttributeError
-        print(num_search)
-        break
+    if num not in range(0,1000000001):
+        print(int(input("Please enter valid number: ")))
+    # If number in range
     else: 
-        print("Please enter valid number: ")
-        continue
+        # Print number
+        print(num)
+        break
+
+# TODO: Works but repeating second prompt after entering valid number
+# in cases where user enters invalid number first -- handle
