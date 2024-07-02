4b0739fbc1c4109a73c16e3511f4d1e072ab8bb7
Amber <adm2037@gmail.com>
2024-07-01 23:59:27 -0500

    Section 12 most characters lab: set variables and wrote first drafts of for loop and find_longest function

--- 12_user-input-string-formatting/12_07_most_characters.py
+++ 12_user-input-string-formatting/12_07_most_characters.py
@@ -8,3 +8,27 @@
 #
 # Example Output:
 #     9, greetings
+
+
+# Prompt user for responses
+message = str(input("Please enter a word or phrase: "))
+
+# Set length of messages as it's own variable
+length = len(message)
+
+# Initialize variable to find longest string
+longest = 0
+
+# Declare empty list for storing messages aka user responses
+messages = []
+
+# Define a loop to run prompt 3 times
+for index in range(3):
+    # add response to list
+    messages.append(message)
+
+def find_longest(message):
+    if len(message).max():
+        print(f"{length} + "," + {message}")
+
+print(messages) # TODO: Fix for loop -- currently just adding response to list 3 times
