bd0ac0cc934e1428bc752b0f752203ef4e3b581d
Amber <adm2037@gmail.com>
2024-07-02 16:56:08 -0500

    Created new branch to work in and prompted user for response

--- 12_user-input-string-formatting/12_09_number_search.py
+++ 12_user-input-string-formatting/12_09_number_search.py
@@ -1,3 +1,6 @@
 # Ask your user for a number between 0 and 1,000,000,000.
 # Use a `while` loop to find the number. When the number is found,
 # exit the loop and print the number to the console.
+
+# Prompt user for number within specified range
+num = int(input("Please enter a number between 0 and 1,000,000,000: "))
