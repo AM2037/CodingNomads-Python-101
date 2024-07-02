6bfa2725eccdb9151dc4eafdb3100695fd4a25a9
Amber <adm2037@gmail.com>
2024-07-02 00:34:45 -0500

    Debugging month name assignment code in section 12 month reminder lab

--- 12_user-input-string-formatting/12_08_month_reminder.py
+++ 12_user-input-string-formatting/12_08_month_reminder.py
@@ -4,5 +4,15 @@
 # Print "Error" if the number from the user is not between 1 and 12.
 # Use a nested `if` statement.
 
+# Prompt user for number between 1 and 12
+mon = int(input("Please enter a number between (or equal to) 1 and 12: "))
 # Assigning month name to number using enumerate 
 mon_names = ['January', 'February', 'March', 'April',  'May',  'June',  'July',  'August',  'September',  'October',  'November',  'December']
+
+mon = [mon_names[x] for x in mon] # TODO: Fix TypeError: 'int' object is not iterable
+assign_names = {x: ind for ind, x in enumerate(set(mon_names))}
+
+# Using nested if statement
+for i in range(1,13):
+    if 1 <= mon <= 12:
+        print(mon_names[i])
