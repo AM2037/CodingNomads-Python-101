9bf43eff4f57a949618186293942fd4a990f54b0
Amber <adm2037@gmail.com>
2024-07-02 01:01:25 -0500

    Completed section 12 month reminder lab: used dictionary and while loop approaches instead of nested if statement

--- 12_user-input-string-formatting/12_08_month_reminder.py
+++ 12_user-input-string-formatting/12_08_month_reminder.py
@@ -2,17 +2,32 @@
 # and print the name of the associated month:
 # "January", "February", ... "December"
 # Print "Error" if the number from the user is not between 1 and 12.
-# Use a nested `if` statement.
+# Use a nested `if` statement. -- Tried this, changing approach to be more concise
 
-# Prompt user for number between 1 and 12
-mon = int(input("Please enter a number between (or equal to) 1 and 12: "))
-# Assigning month name to number using enumerate 
-mon_names = ['January', 'February', 'March', 'April',  'May',  'June',  'July',  'August',  'September',  'October',  'November',  'December']
+# Assigning month numbers to their respective names via dictionary instead
+months = {
+    1: 'January',
+    2: 'February',
+    3: 'March',
+    4: 'April',
+    5: 'May',
+    6: 'June',
+    7: 'July',
+    8: 'August',
+    9: 'September',
+    10:  'October',
+    11:  'November',
+    12: 'December'}
 
-mon = [mon_names[x] for x in mon] # TODO: Fix TypeError: 'int' object is not iterable
-assign_names = {x: ind for ind, x in enumerate(set(mon_names))}
+# If number user entered is in range, print corresponding month name
+while True:
+    # Prompt user for number between 1 and 12
+    mon = int(input("Please enter a number between (or equal to) 1 and 12: "))
 
-# Using nested if statement
-for i in range(1,13):
     if 1 <= mon <= 12:
-        print(mon_names[i])
+        print(months[mon])
+        break
+    else:
+        print("Please enter a valid number.")
+    # Repeat loop
+    continue
