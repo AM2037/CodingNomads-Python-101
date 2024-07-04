8d16fd2459ee0efab0301307425dcaa457ebd392
Amber <adm2037@gmail.com>
2024-07-03 22:23:44 -0500

    Prompted user for opinion and converted letters to lowercase

--- 12_user-input-string-formatting/12_10_sarcastic_prompt.py
+++ 12_user-input-string-formatting/12_10_sarcastic_prompt.py
@@ -1,4 +1,9 @@
 # Create a sarcastic program that asks a user for their honest opinion,
 # then prints the same sentence back to them in aLtErNaTiNg CaPs.
 
-# Prompt user for their honest opinion about something
+'''
+The following prompts user for their honest opinion about something and
+then converts all the letters to lowercase for proper alternating caps.
+'''
+
+op = input("What do you honestly think about pinapple on pizza?").lower()
