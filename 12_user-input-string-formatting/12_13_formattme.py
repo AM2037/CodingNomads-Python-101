a9d3d17e9f560f13d69a12ac64839c681d056afb
Amber <adm2037@gmail.com>
2024-07-03 22:44:42 -0500

    Completed lab by fixing blurb

--- 12_user-input-string-formatting/12_13_formattme.py
+++ 12_user-input-string-formatting/12_13_formattme.py
@@ -3,11 +3,7 @@
 # Then, change the that way you're displaying the multi-line string
 # so that the output doesn't show any superfluous spacing.
 
-# TODO: Fix me
-animal = None
-blurb = f"The {animal} (Felis {animal}us) is a domestic species of small \
-        carnivorous mammal. It is the only domesti{animal}ed species \
-        in the family Felidae and is often referred to as the \
-        domestic {animal} to distinguish it from the wild members of the family."
+animal = "Felid"
+new_blurb = (f"The {animal} (Felis {animal}us) is a domestic species of small carnivorous mammals. It is the only domesticated species in the family Felidae and is often referred to as the domestic {animal} to distinguish it from the wild members of the family.")
 
-print(blurb)
+print(new_blurb)
