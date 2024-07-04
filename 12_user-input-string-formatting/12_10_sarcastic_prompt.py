c961d58d2124324b9f4225620f2972e8dd6d7195
Amber <adm2037@gmail.com>
2024-07-03 21:52:48 -0500

    Created new branch to work in

--- 12_user-input-string-formatting/12_10_sarcastic_prompt.py
+++ 12_user-input-string-formatting/12_10_sarcastic_prompt.py
@@ -1,2 +1,4 @@
 # Create a sarcastic program that asks a user for their honest opinion,
 # then prints the same sentence back to them in aLtErNaTiNg CaPs.
+
+# Prompt user for their honest opinion about something
